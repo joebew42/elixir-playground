af47a01111182b3e33d3497a16700eaffe25b3c7
JoeBew42 <joebew42@gmail.com>
2017-08-11 01:06:43 +0200

    wip on gen server: clean up

--- examples/simple_gen_server/lib/simple_gen_server.ex
+++ examples/simple_gen_server/lib/simple_gen_server.ex
@@ -1,18 +1,2 @@
 defmodule SimpleGenServer do
-  @moduledoc """
-  Documentation for SimpleGenServer.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SimpleGenServer.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- examples/simple_gen_server/test/simple_gen_server_test.exs
+++ examples/simple_gen_server/test/simple_gen_server_test.exs
@@ -1,8 +1,4 @@
 defmodule SimpleGenServerTest do
   use ExUnit.Case
   doctest SimpleGenServer
-
-  test "greets the world" do
-    assert SimpleGenServer.hello() == :world
-  end
 end
