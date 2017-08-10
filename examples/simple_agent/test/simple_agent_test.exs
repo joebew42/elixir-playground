d0873f6c93a9f859181f4915107694ef47f5d8e4
JoeBew42 <joebew42@gmail.com>
2017-08-11 00:53:24 +0200

    using ExUnit callback to remove duplication

--- examples/simple_agent/test/simple_agent_test.exs
+++ examples/simple_agent/test/simple_agent_test.exs
@@ -2,15 +2,16 @@ defmodule SimpleAgentTest do
   use ExUnit.Case, async: true
   doctest SimpleAgent
 
-  test "returns nil when key does not exists" do
+  setup do
     {:ok, agent} = start_supervised SimpleAgent
+    %{agent: agent}
+  end
 
+  test "returns nil when key does not exists", %{agent: agent} do
     assert nil == SimpleAgent.get(agent, "not_existing_key")
   end
 
-  test "store a value in the agent" do
-    {:ok, agent} = start_supervised SimpleAgent
-
+  test "store a value in the agent", %{agent: agent} do
     SimpleAgent.put(agent, "key", "value")
 
     assert "value" == SimpleAgent.get(agent, "key")
