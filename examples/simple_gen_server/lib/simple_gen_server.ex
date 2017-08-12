5b9d63f2581fbfda0e8b767847ebfa580a519cca
JoeBew42 <joebew42@gmail.com>
2017-08-12 14:59:40 +0200

    wip on gen server: add put feature

--- examples/simple_gen_server/lib/simple_gen_server.ex
+++ examples/simple_gen_server/lib/simple_gen_server.ex
@@ -12,15 +12,31 @@ defmodule SimpleGenServer do
     GenServer.call(pid, {:create, name})
   end
 
+  def put(pid, process_name, message) do
+    GenServer.call(pid, {:put, process_name, message})
+  end
+
   def init(names) do
     {:ok, names}
   end
 
   def handle_call({:create, name}, _from, names) do
-    if Map.has_key?(names, name) do
+    if registered?(name, names) do
       {:reply, :already_registered, names}
     else
       {:reply, :ok, Map.put(names, name, true)}
     end
+  end
+
+  def handle_call({:put, name, _message}, _from, names) do
+    if registered?(name, names) do
+      {:reply, :ok, names}
+    else
+      {:reply, :process_not_found, names}
+    end
+  end
+
+  def registered?(name, names) do
+    Map.has_key?(names, name)
   end
 end
--- examples/simple_gen_server/test/simple_gen_server_test.exs
+++ examples/simple_gen_server/test/simple_gen_server_test.exs
@@ -3,17 +3,31 @@ defmodule SimpleGenServerTest do
   doctest SimpleGenServer
 
   setup do
-    {:ok, pid} = start_supervised SimpleGenServer
-    %{server: pid}
+    {:ok, registry} = start_supervised SimpleGenServer
+    %{server: registry}
   end
 
-  test "returns :ok when a new name is registered", %{server: pid} do
-    assert :ok == SimpleGenServer.create(pid, "a_new_name")
+  test "returns :ok when a new name is registered", %{server: registry} do
+    assert :ok == SimpleGenServer.create(registry, "a_new_name")
   end
 
-  test "returns :already_registered when a name is already registered", %{server: pid} do
-    SimpleGenServer.create(pid, "same_name")
+  test "returns :already_registered when a name is already registered", %{server: registry} do
+    SimpleGenServer.create(registry, "same_name")
 
-    assert :already_registered == SimpleGenServer.create(pid, "same_name")
+    assert :already_registered == SimpleGenServer.create(registry, "same_name")
   end
+
+  test ":process_not_found when trying to send a message to a non registered process", %{server: registry} do
+    response = SimpleGenServer.put(registry, "non_registered_process", "a_message")
+
+    assert :process_not_found == response
+  end
+
+  test ":ok when a message is saved to a registered process", %{server: registry} do
+    SimpleGenServer.create(registry, "a_registered_process")
+    response = SimpleGenServer.put(registry, "a_registered_process", "a_message")
+
+    assert :ok == response
+  end
+
 end
