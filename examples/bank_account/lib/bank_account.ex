e2dbddf97d0e8552cfcae4e76a889c1b083442c1
JoeBew42 <joebew42@gmail.com>
2017-09-08 20:43:36 +0200

    separate BankAccount server from the client

--- examples/bank_account/lib/bank_account.ex
+++ examples/bank_account/lib/bank_account.ex
@@ -1,6 +1,6 @@
 defmodule BankAccount do
   def start() do
-    spawn(fn -> loop(1000) end)
+    spawn(fn -> BankAccountServer.loop(1000) end)
   end
 
   def stop(pid) do
@@ -25,46 +25,4 @@ defmodule BankAccount do
       reply -> reply
     end
   end
-
-  defp loop(balance) do
-    receive do
-      {from, message} ->
-        {response, new_balance} = handle(message, balance)
-        send from, response
-        loop(new_balance)
-    end
-  end
-
-  defp handle({:deposit, amount}, balance) do
-    server_deposit(amount, balance)
-  end
-
-  defp handle({:withdraw, amount}, balance) do
-    server_withdraw(amount, balance)
-  end
-
-  defp handle({:check_balance}, balance) do
-    {balance, balance}
-  end
-
-  defp server_deposit(amount, balance) when amount > 0 do
-    {:ok, balance + amount}
-  end
-
-  defp server_deposit(_amount, balance) do
-    {:ok, balance}
-  end
-
-  defp server_withdraw(amount, balance) when amount >= 0 do
-    new_balance = balance - amount
-    case new_balance >= 0 do
-      true -> {:ok, new_balance}
-      false -> {{:error, :withdrawal_not_permitted}, balance}
-    end
-  end
-
-  defp server_withdraw(_amount, balance) do
-    {{:error, :withdrawal_not_permitted}, balance}
-  end
-
 end
