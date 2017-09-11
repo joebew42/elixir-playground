a13168058078a9b3fd531503b336a1f016887ca3
JoeBew42 <joebew42@gmail.com>
2017-09-11 20:31:54 +0200

    decouple domain logic from gen_server

--- examples/bank_account_otp/lib/bank_server.ex
+++ examples/bank_account_otp/lib/bank_server.ex
@@ -6,75 +6,80 @@ defmodule BankServer do
   end
 
   def handle_call({:create_account, account}, _from, accounts) do
-    create_account(account, accounts)
+    {response, state} = create_account(account, accounts)
+    {:reply, response, state}
   end
 
   def handle_call({:delete_account, account}, _from, accounts) do
-    delete_account(account, accounts)
+    {response, state} = delete_account(account, accounts)
+    {:reply, response, state}
   end
 
   def handle_call({:check_balance, account}, _from, accounts) do
-    check_balance(account, accounts)
+    {response, state} = check_balance(account, accounts)
+    {:reply, response, state}
   end
 
   def handle_call({:deposit, amount, account}, _from, accounts) do
-    deposit(amount, account, accounts)
+    {response, state} = deposit(amount, account, accounts)
+    {:reply, response, state}
   end
 
   def handle_call({:withdraw, amount, account}, _from, accounts) do
-    withdraw(amount, account, accounts)
+    {response, state} = withdraw(amount, account, accounts)
+    {:reply, response, state}
   end
 
   defp create_account(account, accounts) do
     case exists?(account, accounts) do
-      true -> {:reply, {:error, :account_already_exists}, accounts}
+      true -> {{:error, :account_already_exists}, accounts}
       false ->
         bank_account = BankAccount.start()
         new_accounts = Map.put(accounts, account, bank_account)
-        {:reply, {:ok, :account_created}, new_accounts}
+        {{:ok, :account_created}, new_accounts}
     end
   end
 
   defp delete_account(account, accounts) do
     case exists?(account, accounts) do
-      false -> {:reply, {:error, :account_not_exists}, accounts}
+      false -> {{:error, :account_not_exists}, accounts}
       true ->
         bank_account = Map.get(accounts, account)
         if BankAccount.stop(bank_account) do
-          {:reply, {:ok, :account_deleted}, Map.delete(accounts, account)}
+          {{:ok, :account_deleted}, Map.delete(accounts, account)}
         else
-          {:reply, {:ok, :account_deleted}, accounts}
+          {{:ok, :account_deleted}, accounts}
         end
     end
   end
 
   defp check_balance(account, accounts) do
     case exists?(account, accounts) do
-      false -> {:reply, {:error, :account_not_exists}, accounts}
+      false -> {{:error, :account_not_exists}, accounts}
       true ->
         bank_account = Map.get(accounts, account)
         response = BankAccount.check_balance(bank_account)
-        {:reply, {:ok, response}, accounts}
+        {{:ok, response}, accounts}
     end
   end
 
   defp deposit(amount, account, accounts) do
     case exists?(account, accounts) do
-      false -> {:reply, {:error, :account_not_exists}, accounts}
+      false -> {{:error, :account_not_exists}, accounts}
       true ->
         bank_account = Map.get(accounts, account)
         BankAccount.deposit(bank_account, amount)
-        {:reply, :ok, accounts}
+        {:ok, accounts}
     end
   end
 
   defp withdraw(amount, account, accounts) do
     case exists?(account, accounts) do
-      false -> {:reply, {:error, :account_not_exists}, accounts}
+      false -> {{:error, :account_not_exists}, accounts}
       true ->
         bank_account = Map.get(accounts, account)
         response = BankAccount.withdraw(bank_account, amount)
-        {:reply, response, accounts}
+        {response, accounts}
     end
   end
 
--- examples/bank_account_otp/scratch.md
+++ examples/bank_account_otp/scratch.md
@@ -1,10 +1,10 @@
 # DOING
 
-
 # TODO
 
-* decouple the GenServer from our application domain logic
+* introduce OTP for the BankAccount
 
 # DONE
 
+* decouple the GenServer from our application domain logic
 * introduce OTP gen_server for the BankServer
