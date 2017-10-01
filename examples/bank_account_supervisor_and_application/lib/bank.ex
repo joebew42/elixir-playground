d2da7460100332345050eeb2d54eae2754024574
JoeBew42 <joebew42@gmail.com>
2017-10-01 20:58:15 +0200

    rename execute in send

--- examples/bank_account_supervisor_and_application/lib/bank.ex
+++ examples/bank_account_supervisor_and_application/lib/bank.ex
@@ -1,25 +1,25 @@
 defmodule Bank do
   def create_account(account) do
-    execute({:create_account, account})
+    send({:create_account, account})
   end
 
   def delete_account(account) do
-    execute({:delete_account, account})
+    send({:delete_account, account})
   end
 
   def check_balance(account) do
-    execute({:check_balance, account})
+    send({:check_balance, account})
   end
 
   def deposit(amount, account) do
-    execute({:deposit, amount, account})
+    send({:deposit, amount, account})
   end
 
   def withdraw(amount, account) do
-    execute({:withdraw, amount, account})
+    send({:withdraw, amount, account})
   end
 
-  defp execute(message) do
+  defp send(message) do
     GenServer.call(:bank_server, message)
   end
 end
