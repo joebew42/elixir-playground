16cc2c67ee5cb05e5648bad5472155695473ffcc
JoeBew42 <joebew42@gmail.com>
2017-12-06 15:09:20 +0100

    add other spec

--- examples/types_specs/lib/types_specs.ex
+++ examples/types_specs/lib/types_specs.ex
@@ -7,8 +7,12 @@ defmodule TypesSpecs do
   @type user :: %{name: String.t, email: String.t}
   @type users :: [user]
 
-  @spec find_by_id(String.t) :: users
-  def find_by_id(user_id) do
+  @spec find_by_id(number) :: users | []
+  def find_by_id(user_id) when user_id > 0 do
     [%{name: "joe", email: "joe@somewhere.com"}]
+  end
+
+  def find_by_id(_user_id) do
+    []
   end
 end
