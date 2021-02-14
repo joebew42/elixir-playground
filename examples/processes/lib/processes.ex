c203f1f896b5d6a1f89d6102a0c3ae17b2182187
Joe Bew <joebew42@gmail.com>
2021-02-14 18:02:51 +0100

    Code style

--- examples/processes/lib/processes.ex
+++ examples/processes/lib/processes.ex
@@ -1,11 +1,11 @@
 defmodule Processes do
-
   def start do
     spawn(fn -> loop(%{balance: 1000}) end)
   end
 
   def call(pid, {:get, :account_balance}) do
     send(pid, {self(), {:get, :account_balance}})
+
     receive do
       account_balance -> account_balance
     end
@@ -13,6 +13,7 @@ defmodule Processes do
 
   def call(pid, {:deposit, :account_balance, amount}) do
     send(pid, {self(), {:deposit, :account_balance, amount}})
+
     receive do
       reply -> reply
     end
@@ -23,12 +24,14 @@ defmodule Processes do
       {from, {:deposit, :account_balance, amount}} ->
         current_amount = Map.get(account, :balance)
         updated_account = Map.put(account, :balance, current_amount + amount)
-        send from, :ok
+        send(from, :ok)
         loop(updated_account)
+
       {from, {:get, :account_balance}} ->
         balance = Map.get(account, :balance)
-        send from, balance
+        send(from, balance)
         loop(account)
+
       _ ->
         loop(account)
     end
