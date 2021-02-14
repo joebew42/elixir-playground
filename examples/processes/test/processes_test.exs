da1ba0c9dbab7dae3a102a14248988021f1aaacb
Joe Bew <joebew42@gmail.com>
2021-02-14 18:02:23 +0100

    Code style

--- examples/processes/test/processes_test.exs
+++ examples/processes/test/processes_test.exs
@@ -2,13 +2,13 @@ defmodule ProcessesTest do
   use ExUnit.Case
 
   test "process is alive once is started" do
-    pid = Processes.start
+    pid = Processes.start()
 
-    assert true == Process.alive? pid
+    assert true == Process.alive?(pid)
   end
 
   test "return the current account balance" do
-    pid = Processes.start
+    pid = Processes.start()
 
     response = Processes.call(pid, {:get, :account_balance})
 
@@ -16,7 +16,7 @@ defmodule ProcessesTest do
   end
 
   test "deposit 100 will increase the account balance to 1100" do
-    pid = Processes.start
+    pid = Processes.start()
     Processes.call(pid, {:deposit, :account_balance, 100})
     response = Processes.call(pid, {:get, :account_balance})
 
