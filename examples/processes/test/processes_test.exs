29ad6479eb0645c79b84df5b2105bb599a8ef196
Joe Bew <joebew42@gmail.com>
2021-02-17 16:03:26 +0100

    Add an example for a blog about learning with tests

--- examples/processes/test/processes_test.exs
+++ examples/processes/test/processes_test.exs
@@ -1,6 +1,59 @@
 defmodule ProcessesTest do
   use ExUnit.Case
 
+  defmodule MyProcess do
+    def start(language \\ :en) do
+      spawn(MyProcess, :loop, [language])
+    end
+
+    def greet(pid) do
+      send(pid, {self(), :greet})
+
+      receive do
+        response -> response
+      end
+    end
+
+    def loop(language \\ :en) do
+      receive do
+        {from, :greet} ->
+          message =
+            case language do
+              :es -> "Hola mundo!"
+              _ -> "Hello world!"
+            end
+
+          send(from, message)
+          loop(language)
+
+        _ ->
+          loop(language)
+      end
+    end
+  end
+
+  test "run a process" do
+    pid = MyProcess.start()
+
+    assert Process.alive?(pid)
+  end
+
+  test "returns 'Hello world!' when receive :greet" do
+    pid = MyProcess.start()
+
+    response = MyProcess.greet(pid)
+
+    assert response == "Hello world!"
+  end
+
+  test "returns 'Hola mundo!' when receive :greet and starts with :es" do
+    pid = MyProcess.start(:es)
+
+    response = MyProcess.greet(pid)
+
+    assert response == "Hola mundo!"
+  end
+
   test "process is alive once is started" do
     pid = Processes.start()
 
